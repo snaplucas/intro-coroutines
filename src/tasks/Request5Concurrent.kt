d2f2b0526f6b60ed4dd75ffb561f0cacf4b27a11
snaplucas <lucas.chapola@gmail.com>
2022-06-15 23:28:47 +0200

    implementing Aggregation and Request5Concurrent

--- src/tasks/Request5Concurrent.kt
+++ src/tasks/Request5Concurrent.kt
@@ -4,5 +4,16 @@ import contributors.*
 import kotlinx.coroutines.*
 
 suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData): List<User> = coroutineScope {
-    TODO()
+    val repos = service.getOrgRepos(req.org) // Executes request and blocks the current thread
+        .also { logRepos(req, it) }
+        .body() ?: listOf()
+
+    val allUsers = repos.map { repo ->
+        async {
+            service.getRepoContributor(req.org, repo.name)
+                .also { logUsers(repo, it) }
+                .body() ?: listOf()
+        }
+    }.awaitAll().flatten()
+    allUsers.aggregate()
 }
